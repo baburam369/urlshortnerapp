d7fcc0744ce406f4197cc65fd3d239c8656f41bb
eigma <baburamupadhaya126@gmail.com>
2022-03-02 01:11:17 +0530

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bp = require("body-parser");
 const mongoose = require("mongoose");
 const short = require("short-uuid");
 const ShortUniqueId = require("short-unique-id");
-require('dotenv').config()
+require("dotenv").config();
 const url =
-  "mongodb+srv://urlshortner:"+process.env.DB_PASSWORD+"@urlshortner.feten.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://urlshortner:" +
+  process.env.DB_PASSWORD +
+  "@urlshortner.feten.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(
   url,
@@ -34,17 +36,29 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { shorturl: null });
 });
-app.post("/shorturl", (req, res) => {
+app.post("/", (req, res) => {
   const uid = new ShortUniqueId();
   const uuid = uid(6);
-  
-  const newurl = new Url({ url: req.body.url, shorturl: uuid });
-  newurl.save((err, doc) => {
+
+  Url.findOne({ url: req.body.url }, (err, data) => {
     if (err) {
-      console.log(err);
-    } else {
+      return res.render("index", {
+        shorturl: err,
+      });
+    } else if (!data) {
+      const newurl = new Url({ url: req.body.url, shorturl: uuid });
+      newurl.save((err, doc) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("index", {
+            shorturl: "http://" + req.headers.host + "/" + doc.shorturl,
+          });
+        }
+      });
+    }else{
       res.render("index", {
-        shorturl: "http://" + req.headers.host + "/" + doc.shorturl,
+        shorturl: "http://" + req.headers.host + "/" + data.shorturl,
       });
     }
   });
@@ -57,7 +71,7 @@ app.get("/:shorturl", (req, res) => {
   });
 });
 
-let port = process.env.PORT||"3000;"
+let port = process.env.PORT || "3000;";
 app.listen(port, (err) => {
   if (!err) return console.log("Server started");
 });
